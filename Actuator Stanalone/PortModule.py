1f08b024cfacd3363a406f7927ef7a60f78c1245
Cheer3142 <cheer3142@gmail.com>
2024-05-14 10:59:38 +0700

    Update PortModule.py

--- Actuator Stanalone/PortModule.py
+++ Actuator Stanalone/PortModule.py
@@ -29,7 +29,6 @@ def serial_ports():
     if sys.platform.startswith('win'):
         ports = ['COM%s' % (i + 1) for i in range(256)]
     elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
-        # this excludes your current terminal "/dev/tty"
         ports = glob.glob('/dev/tty[A-Za-z]*')
     elif sys.platform.startswith('darwin'):
         ports = glob.glob('/dev/tty.*')
